be6030862cf8a33eee76aa8ad0baaf0e37294c8a
Yvan C <magic.yvan@gmail.com>
2026-05-16 11:50:09 +0200

    Add color change on click with random RGB values

--- ch62/color.js
+++ ch62/color.js
@@ -1,1 +1,11 @@
+const color = document.getElementById("color");
+const target = document.getElementById("result");
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }  
+
+color.onclick = () => {
+    window.location.replace('https://keskispace.free.beeceptor.com/');
+    target.style.color = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
+}
